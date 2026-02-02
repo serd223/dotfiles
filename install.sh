3563993ae184ddd97460f5b7096f89b19549cf00
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2026-02-02 23:58:12 +0300

    add backup functionality

--- install.sh
+++ install.sh
@@ -1,21 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd ~/dotfiles
+mkdir -p ./bak/
 
+# bash
+mkdir -p ./bak/bash/
+
+cp ~/.bash_aliases ./bak/bash/
 cp ./bash/.bash_aliases ~/
+
+cp ~/.bashrc ./bak/bash/
 cp ./bash/.bashrc ~/
+# /bash
 
+# helix
 mkdir -p ~/.config/helix/
+mkdir -p ./bak/helix/
+
+cp ~/.config/helix/config.toml ./bak/helix/
 cp ./helix/config.toml ~/.config/helix/
+
+cp ~/.config/helix/languages.toml ./bak/helix/
 cp ./helix/languages.toml ~/.config/helix/
+# /helix
 
+# walked
 mkdir -p ~/.config/walked/
+mkdir -p ./bak/walked/
+
+cp ~/.config/walked/walked.toml ./bak/walked/
 cp ./walked/walked.toml ~/.config/walked/
+# /walked
 
+# tmux
+mkdir -p ./bak/tmux/
+
+cp ~/.tmux.conf ./bak/tmux/
 cp ./tmux/.tmux.conf ~/
+# /tmux
 
+# fastfetch
 mkdir -p ~/.config/fastfetch/
-cp ./fastfetch/config.jsonc ~/.config/fastfetch/
+mkdir -p ./bak/fastfetch/
 
-echo "To apply changes, run:"
-echo "    source ~/.bashrc"
-echo "    tmux source-file ~/.tmux.conf (if inside tmux session)"
+cp ~/.config/fastfetch/config/jsonc ./bak/fastfetch/
+cp ./fastfetch/config.jsonc ~/.config/fastfetch/
+# /fastfetch
